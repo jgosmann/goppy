e2729943022a97f26c025b204abebdb177e867cb
Jan Gosmann <jan@hyper-world.de>
2014-02-02 12:04:41 -0500

    Basic GrowableArray implementation.

--- goppy/growable.py
+++ goppy/growable.py
@@ -3,19 +3,33 @@
 import numpy as np
 
 
-def GrowableArray(shape, dtype=None, order='C'):
-    return _GrowableArray.create(shape, dtype, order)
+# TODO
+# ok, plan is no to return a class derived from ndarray which is able to detect
+# whether it is the originally created instance or a view. If it is a view
+# the grow_by function will throw an exception.
 
-
-class _GrowableArray(np.ndarray):
-    def __new__(cls, *args, **kwargs):
-        return np.ndarray.__new__(np.ndarray, *args, **kwargs)
+class GrowableArray(object):
+    def __init__(self, shape, dtype=float, order='C'):
+        self._data = np.empty(shape, dtype, order)
+        self._view = self.__get_view_for_shape(self._data, shape)
 
     @staticmethod
-    def create(shape, dtype, order):
-        data = np.ndarray(shape=shape, dtype=dtype, order=order)
-        view = np.ndarray.__new__(
-            _GrowableArray, shape=shape, dtype=dtype, buffer=data,
-            strides=data.strides, order=order)
-        setattr(view, '_data', data)
-        return view
+    def __get_view_for_shape(data, shape):
+        return data.__getitem__([slice(end) for end in shape])
+
+    def __getattr__(self, name):
+        return getattr(self._view, name)
+
+    def __getitem__(self, key):
+        return self._view.__getitem__(key)
+
+    def __setitem__(self, key, value):
+        self._view.__setitem__(key, value)
+
+    def grow_by(self, amount):
+        amount = np.asarray(amount, dtype=int)
+        assert np.all(amount > 0)
+        new_shape = np.asarray(self.shape, dtype=int) + amount
+        self._view = None
+        self._data.resize(new_shape)
+        self._view = self.__get_view_for_shape(self._data, new_shape)
--- goppy/test/test_growable.py
+++ goppy/test/test_growable.py
@@ -12,41 +12,46 @@ class TestGrowableArray(object):
     def test_creation_of_array(self):
         shape = (2, 3, 4)
         dtype = np.dtype('int')
-        garray = GrowableArray(shape, dtype)
+        order = 'C'
+        garray = GrowableArray(shape, dtype, order)
 
         assert_that(garray.shape, is_(shape))
         assert_that(garray.dtype, is_(dtype))
+        assert_that(garray.flags['C'], is_(True))
 
     def test_array_access(self):
         garray = GrowableArray((2, 2))
         garray[:, :] = 3
         assert_array_equal(garray, [[3, 3], [3, 3]])
 
-    # FIXME should not be possible
-    def test_view_casting(self):
-        array = np.array([[1, 2], [3, 4]])
-        garray = array.view(GrowableArray)
+    #def test_view_casting(self):
+        #array = np.array([[1, 2], [3, 4]])
+        #garray = array.view(GrowableArray)
+        #assert_that(garray, is_(instance_of(GrowableArray)))
+        #assert_array_equal(garray, array)
 
-        assert_that(garray, is_(instance_of(GrowableArray)))
-        assert_array_equal(garray, array)
+    #def test_view_casted_cannot_be_grown(self):
+        #pass  # FIXME
 
-    # FIXME content should match
     def test_new_from_template(self):
         garray = GrowableArray((3,))
         garray[:] = [1, 2, 3]
         new_one = garray[1:]
-        print type(new_one), id(new_one.base), id(garray.base), id(garray)
-        # FIXME produces another GrowableArray which should not be the case.
-        # Resizing would not work as expected. But the _data attribute could be
-        # used to ensure that its not only a view.
 
         assert_that(new_one, is_not(same_instance(garray)))
-        assert_that(new_one, is_(instance_of(GrowableArray)))
+        assert_array_equal(new_one, [2, 3])
 
-    #def test_initializes_buffer_shape_with_twice_initial_sizes(self):
-        #array = GrowableArray((2, 2, 3))
-        #assert_that(array.buffer_size, is_(equal_to((4, 4, 6))))
+    def test_new_from_template_cannot_be_grown(self):
+        pass  # FIXME
 
+    def test_can_grow_array(self):
+        garray = GrowableArray((2, 2))
+        garray.grow_by((10, 20))
+        garray[:, :] = 1
+        assert_array_equal(garray, np.ones((12, 22)))
+
+    def test_invalid_assignment(self):
+        pass  # FIXME
 
     # enlarge view casted array
     # enlarge new from template
