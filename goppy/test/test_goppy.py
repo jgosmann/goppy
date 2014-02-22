adf243ad422bba10c5c3467d2ae79626041706d1
Jan Gosmann <jan@hyper-world.de>
2014-02-22 15:31:27 -0500

    Remove uneeded commented code.

--- goppy/test/test_goppy.py
+++ goppy/test/test_goppy.py
@@ -94,27 +94,6 @@ class TestOnlineGP(object):
         gp.fit(data['X'], data['Y'])
         assert_that(gp.trained, is_(True))
 
-    #def test_allows_adding_new_datapoints_online(self):
-        #xs = [-4, -2, -0.5, 0, 2]
-        #ys = [-2, 0, 1, 2, -1]
-
-        #for x, y, in zip(xs, ys):
-            #self.gp.add_observations(np.array([[x]]), np.array([[y]]))
-
-        #x_star = np.array([[-3, 1]]).T
-        #expected = np.array([[-0.78511166, 0.37396387]]).T
-        #expected_mse = [1.04585738, 1.04888027]
-        #pred, mse = self.gp.predict(x_star, eval_MSE=True)
-        #assert_almost_equal(pred, expected)
-        #assert_almost_equal(mse, expected_mse)
-
-    #def test_has_trained_indicator(self):
-        #assert_that(self.gp.trained, is_(False))
-        #x = np.array([[-4, -2, -0.5, 0, 2]]).T
-        #y = np.array([[-2, 0, 1, 2, -1]]).T
-        #self.gp.fit(x, y)
-        #assert_that(self.gp.trained, is_(True))
-
     #def test_can_calculate_prediction_derivative(self):
         #x = np.array([[-4, -2, -0.5, 0, 2]]).T
         #y = np.array([[-2, 0, 1, 2, -1]]).T
