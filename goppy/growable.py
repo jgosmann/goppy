f8bec5e60786bb1ad6421d43bb76ab9d544fe107
Jan Gosmann <jan@hyper-world.de>
2014-03-23 16:15:44 -0400

    Rename of GrowableArray member.

--- goppy/growable.py
+++ goppy/growable.py
@@ -38,11 +38,11 @@ class GrowableArray(object):
      [ 2.  2.  2.]]
     """
 
-    MARGIN_FACTOR = 2
+    ENLARGE_FACTOR = 2
 
     def __init__(self, shape, dtype=float, order='C', buffer_shape=None):
         if buffer_shape is None:
-            buffer_shape = self.MARGIN_FACTOR * np.asarray(shape, dtype=int)
+            buffer_shape = self.ENLARGE_FACTOR * np.asarray(shape, dtype=int)
         self._data = np.empty(buffer_shape, dtype, order)
         self._view = self.__get_view_for_shape(self._data, shape)
 
@@ -87,6 +87,6 @@ class GrowableArray(object):
         new_shape = np.asarray(self.shape, dtype=int) + amount
         # TODO unittest data stays at the right place
         if np.any(self._data.shape < new_shape):
-            self._data = np.empty(self.MARGIN_FACTOR * new_shape)
+            self._data = np.empty(self.ENLARGE_FACTOR * new_shape)
             self._data[[slice(None, s) for s in self._view.shape]] = self._view
         self._view = self.__get_view_for_shape(self._data, new_shape)
