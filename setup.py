5bedfc2213b371285982f883f931c988538e6f34
Jan Gosmann <jan@hyper-world.de>
2016-07-17 15:24:12 -0400

    Bump version number.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='GopPy',
-    version='1.0.1',
+    version='1.0.2',
     description='GopPy (Gaussian Online Processes for Python) is a pure '
     'Python module providing a Gaussian process implementation which allows '
     'to efficiently add new data online.',
